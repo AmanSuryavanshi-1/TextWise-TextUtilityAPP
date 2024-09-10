e1da5e88e08cfa16569f95cebccfd4160c59b5a2
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-09-10 14:02:14 +0530

    added styling to contact and auth

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -10,57 +10,57 @@ const Auth = () => {
   };
 
   return (
-    <div className="flex mb-8 items-center justify-center h-[90%] p-4 font-sans bg-bg">
+    <div className="flex items-center justify-center h-[94vh] p-4 font-sans bg-primaryVariant">
       <div className={`relative mt-5 overflow-hidden bg-primary rounded-3xl shadow-custom w-full max-w-[768px] min-h-[480px] ${isActive ? 'active' : ''}`}>
         <div className="absolute top-0 w-full h-full transition-all ease-in-out duration-400">
           
           {/* Sign Up Form */}
           <div className={`absolute top-0 h-full transition-all duration-400 ease-in-out ${isActive ? 'translate-x-0 opacity-100 z-20' : 'translate-x-full opacity-0 z-10'} w-1/2 right-0`}>
             <form onSubmit={(e) => handleSubmit(e, 'signup')} className="flex flex-col items-center justify-center h-full px-6 bg-primary sm:px-10 max-md:px-2">
-              <h1 className="mb-4 font-serif text-2xl font-bold text-white max-md:text-xl">Create Account</h1>
+              <h1 className="mb-4 font-serif text-2xl font-bold text-bg max-md:text-xl">Create Account</h1>
               <div className="w-full mt-4">
-                <button className="flex items-center justify-center w-full px-4 py-2 text-xs font-medium transition-colors bg-white border rounded-lg duration-400 shadow-custom text-text-color border-bg hover:bg-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-bg max-md:px-1">
+                <button className="flex items-center justify-center w-full px-4 py-2 text-xs font-medium transition-colors bg-white border rounded-lg duration-400 shadow-custom text-text-color border-bg hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primaryVariant max-md:px-1">
                   <FcGoogle className="mr-2" /> <span>Continue with Google</span>
                 </button>
               </div>
-              <span className="my-4 text-sm text-center text-white max-md:text-xs">or use your email for registration</span>
-              <input type="text" placeholder="Name" className="w-full px-4 py-2 mb-2 text-sm text-white border-none rounded-lg outline-none bg-bg-variant" />
-              <input type="email" placeholder="Email" className="w-full px-4 py-2 mb-2 text-sm text-white border-none rounded-lg outline-none bg-bg-variant" />
-              <input type="password" placeholder="Password" className="w-full px-4 py-2 mb-2 text-sm text-white border-none rounded-lg outline-none bg-bg-variant" />
-              <button type="submit" className="px-12 py-3 mt-4 text-xs font-semibold tracking-wide uppercase transition-colors rounded-lg cursor-pointer duration-400 text-text-color bg-bg hover:bg-bg-variant">Sign Up</button>
+              <span className="my-4 text-sm text-center text-bgVariant max-md:text-xs">or use your email for registration</span>
+              <input type="text" placeholder="Name" className="w-full px-4 py-2 mb-2 text-sm bg-white border-none rounded-lg outline-none text-bgVariant" />
+              <input type="email" placeholder="Email" className="w-full px-4 py-2 mb-2 text-sm bg-white border-none rounded-lg outline-none text-bgVariant" />
+              <input type="password" placeholder="Password" className="w-full px-4 py-2 mb-2 text-sm bg-white border-none rounded-lg outline-none text-bgVariant" />
+              <button type="submit" className="inline-flex items-center px-8 py-2 mt-4 text-xs font-semibold tracking-wide text-white uppercase transition-all duration-300 border-2 rounded-lg cursor-pointer bg-bg hover:bg-bgVariant border-bg">Sign Up</button>
             </form>
           </div>
 
           {/* Sign In Form */}
           <div className={`absolute top-0 h-full transition-all duration-400 ease-in-out ${isActive ? '-translate-x-full opacity-0' : 'translate-x-0 opacity-100 z-20'} left-0 w-1/2`}>
             <form onSubmit={(e) => handleSubmit(e, 'signin')} className="flex flex-col items-center justify-center h-full px-6 bg-primary sm:px-10 max-md:px-2">
-              <h1 className="mb-4 font-serif text-2xl font-bold text-white max-md:text-xl">Sign In</h1>
+              <h1 className="mb-4 font-serif text-2xl font-bold text-bg max-md:text-xl">Sign In</h1>
               <div className="w-full mt-4">
-                <button className="flex items-center justify-center w-full px-4 py-2 text-xs font-medium transition-colors bg-white border rounded-lg duration-400 shadow-custom text-text-color border-bg hover:bg-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-bg max-md:px-1">
+                <button className="flex items-center justify-center w-full px-4 py-2 text-xs font-medium transition-colors bg-white border rounded-lg duration-400 shadow-custom text-text-color border-bg hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-bg max-md:px-1">
                   <FcGoogle className="mr-2" /> <span>Continue with Google</span>
                 </button>
               </div>
-              <span className="my-4 text-sm text-center text-white max-md:text-xs">or use your email password</span>
-              <input type="email" placeholder="Email" className="w-full px-4 py-2 mb-2 text-sm text-white border-none rounded-lg outline-none bg-bg-variant" />
-              <input type="password" placeholder="Password" className="w-full px-4 py-2 mb-2 text-sm text-white border-none rounded-lg outline-none bg-bg-variant" />
-              <a href="#" className="my-2 text-sm transition-colors duration-400 text-bg-variant hover:text-light">Forgot Your Password?</a>
-              <button type="submit" className="px-12 py-3 mt-4 text-xs font-semibold tracking-wide uppercase transition-colors rounded-lg cursor-pointer duration-400 text-text-color bg-bg hover:bg-bg-variant">Sign In</button>
+              <span className="my-4 text-sm text-center text-bgVariant max-md:text-xs">or use your email password</span>
+              <input type="email" placeholder="Email" className="w-full px-4 py-2 mb-2 text-sm bg-white border-none rounded-lg outline-none text-bgVariant" />
+              <input type="password" placeholder="Password" className="w-full px-4 py-2 mb-2 text-sm bg-white border-none rounded-lg outline-none text-bgVariant" />
+              <a href="#" className="my-2 text-sm transition-colors duration-400 text-bg hover:text-bgVariant">Forgot Your Password?</a>
+              <button type="submit" className="inline-flex items-center px-8 py-2 mt-4 text-xs font-semibold tracking-wide text-white uppercase transition-all duration-300 border-2 rounded-lg cursor-pointer bg-bg hover:bg-bgVariant border-bg">Sign In</button>
             </form>
           </div>
         </div>
 
         {/* Toggle Container */}
         <div className={`absolute top-0 left-1/2 w-1/2 h-full overflow-hidden transition-all duration-400 ease-in-out ${isActive ? '-translate-x-full rounded-r-[150px]' : 'rounded-l-[150px]'} z-50`}>
-          <div className={`bg-gradient-to-r from-bg to-bg-variant text-text-color relative -left-full h-full w-[200%] ${isActive ? 'translate-x-1/2' : 'translate-x-0'} transition-all duration-400 ease-in-out`}>
+          <div className={`bg-gradient-to-r from-bg to-bgVariant text-primaryVariant relative -left-full h-full w-[200%] ${isActive ? 'translate-x-1/2' : 'translate-x-0'} transition-all duration-400 ease-in-out`}>
             <div className={`absolute flex flex-col items-center justify-center px-8 text-center top-0 h-full w-1/2 ${isActive ? 'translate-x-0' : '-translate-x-[200%]'} transition-all duration-400 ease-in-out max-md:px-2`}>
               <h1 className="mb-2 font-serif text-2xl font-bold max-md:text-xl">Welcome Back!</h1>
-              <p className="mb-4 text-sm max-md:text-xs">Enter your personal details to use all of site features</p>
-              <button onClick={() => setIsActive(false)} className="px-12 py-3 text-xs font-semibold tracking-wide uppercase transition-colors bg-transparent border rounded-lg cursor-pointer duration-400 text-text-color border-text-color hover:bg-white hover:text-bg" >Sign In</button>
+              <p className="my-4 text-sm max-md:text-xs">Enter your personal details to use all of site features</p>
+              <button onClick={() => setIsActive(false)} className="px-12 py-3 text-xs font-semibold tracking-wide uppercase transition-colors bg-transparent border rounded-lg cursor-pointer duration-400 border-text-color hover:bg-white hover:text-bg" >Sign In</button>
             </div>
             <div className={`absolute flex flex-col items-center justify-center px-8 text-center top-0 right-0 h-full w-1/2 ${isActive ? 'translate-x-[200%]' : 'translate-x-0'} transition-all duration-400 ease-in-out max-md:px-2`}>
               <h1 className="mb-2 font-serif text-2xl font-bold max-md:text-xl">Hello, Friend!</h1>
-              <p className="mb-4 text-sm max-md:text-xs">Register with your personal details to use all of site features</p>
-              <button onClick={() => setIsActive(true)} className="px-12 py-3 text-xs font-semibold tracking-wide uppercase transition-colors bg-transparent border rounded-lg cursor-pointer duration-400 text-text-color border-text-color hover:bg-white hover:text-bg">Sign Up</button>
+              <p className="my-4 text-sm max-md:text-xs">Register with your personal details to use all of site features</p>
+              <button onClick={() => setIsActive(true)} className="px-12 py-3 text-xs font-semibold tracking-wide uppercase transition-colors bg-transparent border rounded-lg cursor-pointer duration-400 border-text-color hover:bg-white hover:text-bg">Sign Up</button>
             </div>
           </div>
         </div>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsWhatsapp, BsSend } from 'react-icons/bs';
-// import emailjs from 'emailjs-com';
+import emailjs from 'emailjs-com';
 import { Link } from 'react-router-dom';
 
 const Contact = () => {
@@ -23,13 +23,13 @@ const Contact = () => {
   };
 
   return (
-    <div className="flex items-start justify-center mt-8 min-h-[calc(100vh-6rem)] py-8 px-4 sm:px-8 font-sans bg-primary-bgColor">
-      <div className="w-full max-w-[58rem] shadow-2xl">
+    <div className="flex items-start justify-center min-h-[94vh] py-8 px-4 sm:px-8 font-sans bg-primaryVariant">
+      <div className="w-full rounded-3xl my-auto max-w-[58rem] shadow-2xl">
         <div className="w-full h-full transition-all ease-in-out duration-600">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-0">
             {/* Contact Info */}
-            <div className="flex flex-col px-6 py-8 sm:px-12 bg-gradient-to-br from-primary-yellow to-primary-light text-primary-dark rounded-t-3xl md:rounded-l-3xl md:rounded-tr-none">
-              <h2 className="mb-6 font-serif text-2xl font-bold text-center sm:text-3xl">Get in Touch</h2>
+            <div className="flex flex-col px-6 py-8 sm:px-12 bg-bgVariant text-bg rounded-t-3xl md:rounded-l-3xl md:rounded-tr-none">
+              <h2 className="mb-6 font-serif text-2xl font-bold text-center text-white sm:text-3xl">Get in Touch</h2>
               <div className="flex flex-col justify-center flex-grow space-y-6 sm:space-y-10">
                 <ContactCard
                   icon={<HiOutlineMail className="text-3xl sm:text-4xl" />}
@@ -49,8 +49,15 @@ const Contact = () => {
             </div>
 
             {/* Contact Form */}
-            <form ref={form} onSubmit={sendEmail} className="flex flex-col px-6 py-8 sm:px-12 bg-primary-dark rounded-b-3xl md:rounded-r-3xl md:rounded-bl-none">
-              <h2 className="mb-6 font-serif text-2xl font-bold text-center sm:text-3xl text-primary-white">Send us a message</h2>
+            <form ref={form} onSubmit={sendEmail} className="relative flex flex-col px-6 py-8 sm:px-12 bg-gradient-to-br from-primary to-primaryVariant rounded-b-3xl md:rounded-r-3xl md:rounded-bl-none">
+              
+              {isMessageSent && (
+                <p className="absolute top-[-30px] left-0 right-0 mx-auto mb-4 text-center text-sm font-semibold text-primaryVariant bg-bg p-2 rounded-lg shadow-md w-72 animate-fadeIn">
+                  Message sent successfully!
+                </p>
+              )}
+
+              <h2 className="mb-6 font-serif text-2xl font-bold text-center sm:text-3xl text-bg">Send us a message</h2>
               <div className="flex flex-col justify-center flex-grow">
                 <Input name="name" placeholder="Your Full Name" />
                 <Input name="email" type="email" placeholder="Your Email" />
@@ -59,18 +66,14 @@ const Contact = () => {
                   rows="4"
                   placeholder="Your Message"
                   required
-                  className="w-full px-4 py-4 mb-4 text-sm transition-all duration-300 border-2 border-transparent rounded-lg outline-none resize-none bg-primary-grey text-primary-white focus:ring-2 focus:ring-primary-yellow focus:border-primary-yellow"
+                  className="w-full px-4 py-4 mb-4 overflow-y-auto text-sm transition-all duration-300 bg-white border-2 border-transparent rounded-lg outline-none scrollbar-thin scrollbar-thumb-bgVariant scrollbar-track-primary text-bgVariant focus:ring-2 focus:ring-primaryVariant focus:border-bg max-h-40"
                 ></textarea>
                 <button
                   type="submit"
-                  className="flex items-center justify-center px-8 py-2 mx-auto mt-2 text-xs font-semibold tracking-wide uppercase transition-all duration-300 rounded-lg cursor-pointer text-primary-dark bg-primary-yellow hover:bg-yellow-400 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-                >
+                  className="inline-flex items-center justify-center px-6 py-2 mx-16 font-semibold text-white transition-all duration-300 border-2 rounded-full bg-bg hover:bg-bgVariant border-bg">
+                  <BsSend className="mr-2 text-base" />
                   Send Message
-                  <BsSend className="ml-2 text-base" />
                 </button>
-                {isMessageSent && (
-                  <p className="mt-4 text-sm text-center text-green-400 animate-pulse">Message sent successfully!</p>
-                )}
               </div>
             </form>
           </div>
@@ -83,11 +86,11 @@ const Contact = () => {
 
 const ContactCard = ({ icon, title, content, link, linkText }) => (
   <div className="flex items-center p-4 space-x-3 transition-all duration-300 bg-white shadow-md sm:p-5 sm:space-x-4 rounded-xl hover:shadow-lg hover:transform hover:scale-105">
-    <div className="p-3 rounded-full sm:p-4 text-primary-yellow bg-primary-dark">{icon}</div>
+    <div className="m-1 rounded-full sm:p-4 text-primaryVariant bg-bg">{icon}</div>
     <div>
       <h4 className="text-base font-semibold sm:text-lg">{title}</h4>
-      <p className="mb-1 text-xs text-gray-600 sm:text-sm">{content}</p>
-      <Link to={link} className="block text-xs font-semibold transition-colors duration-300 sm:text-sm text-primary-yellow hover:text-primary-dark hover:underline">
+      <p className="mb-1 text-xs text-bgVariant sm:text-sm">{content}</p>
+      <Link to={link} className="block text-xs font-semibold transition-colors duration-300 sm:text-sm text-bg hover:text-primary hover:underline">
         {linkText}
       </Link>
     </div>
@@ -100,7 +103,7 @@ const Input = ({ name, type = "text", placeholder }) => (
     name={name}
     placeholder={placeholder}
     required
-    className="w-full px-4 py-2 mb-4 text-sm transition-all duration-300 border-2 border-transparent rounded-lg outline-none bg-primary-grey text-primary-white focus:ring-2 focus:ring-primary-yellow focus:border-primary-yellow"
+    className="w-full px-4 py-2 mb-4 text-sm transition-all duration-300 bg-white border-2 border-transparent rounded-lg outline-none text-bgVariant focus:ring-2 focus:ring-primaryVariant focus:border-bg"
   />
 );
 
